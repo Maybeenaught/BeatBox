d3fd2fa87288a35754d1f63ff8be3ebc11b29bfa
Maybees <8634709+Maybees@users.noreply.github.com>
2020-03-12 14:48:49 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BeatBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
