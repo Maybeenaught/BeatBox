59394ef750c8bb71ce432e8a0f669a03dc88ae59
Maybees <8634709+Maybees@users.noreply.github.com>
2020-03-12 19:57:32 -0700

    Add project files.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
-namespace BeatBox
+namespace BeatBox.Net
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
