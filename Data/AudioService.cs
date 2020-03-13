9c13b445304a8b1d29a7d21eb546562dcf48956b
Maybees <8634709+Maybees@users.noreply.github.com>
2020-03-12 21:34:17 -0700

    Added NAudio library Added Record button Added basic WASAPI recording functionality

---
+++ Data/AudioService.cs
@@ -0,0 +1,44 @@
+﻿using NAudio.Wave;
+using System;
+using System.IO;
+using System.Threading;
+
+namespace BeatBox.Net.Data
+{
+    public class AudioService
+    {
+        public AudioService() { }
+
+        public void CaptureWasapi()
+        {
+            var outputFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "NAudio");
+            Directory.CreateDirectory(outputFolder);
+            var outputFilePath = Path.Combine(outputFolder, "recorded.wav");
+            var capture = new WasapiLoopbackCapture();
+            var writer = new WaveFileWriter(outputFilePath, capture.WaveFormat);
+
+            capture.DataAvailable += (s, a) =>
+            {
+                writer.Write(a.Buffer, 0, a.BytesRecorded);
+                if (writer.Position > capture.WaveFormat.AverageBytesPerSecond * 20)
+                {
+                    capture.StopRecording();
+                }
+            };
+
+            capture.RecordingStopped += (s, a) =>
+            {
+                writer.Dispose();
+                writer = null;
+                capture.Dispose();
+            };
+
+            capture.StartRecording();
+            while (capture.CaptureState != NAudio.CoreAudioApi.CaptureState.Stopped)
+            {
+                Thread.Sleep(500);
+            }
+        }
+
+    }
+}
